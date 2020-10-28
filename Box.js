6aa3f705a0ad3d7bc752d5b8db040d14d4b8844d
“piyaup” <piyapapadkar@gmail.com>
2020-10-27 21:32:25 -0700

    made space key and tint

---
+++ Box.js
@@ -0,0 +1,20 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+  }
+  display() {
+    console.log(this.body.speed);
+    if(this.body.speed<10){
+      super.display()
+    }
+    else{
+    World.remove(world,this.body)
+    push ();
+    this.visibility = this.visibility-1;
+    tint (255,this.visibility);
+  //  image (this.image,this.body.position.x,this.body.position.y,50,50);
+    pop ();
+  }
+
+}
+}
